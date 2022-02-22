8c80d778c42fc3469cfb9303399febffb1b2278d
Edu M <edumovil84@gmail.com>
2022-02-22 00:20:01 +0000

    refactor calculate.js

--- less2/calculate.js
+++ less2/calculate.js
@@ -4,24 +4,34 @@
 // perform the operatin on the two numbers given
 // print the result to terminal
 const readline = require('readline-sync');
-console.log('Welcome to calculator!');
-console.log("What's the first number?");
+function prompt(message) {
+  console.log(`=> ${message}`); 
+}
+
+
+prompt('Welcome to calculator!');
+prompt("What's the first number?");
 let firstNumber = readline.question();
-console.log("What's the second number?");
+prompt("What's the second number?");
 let secondNumber = readline.question();
 
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
 let output;
-if (operation === '1') {
+switch (operation) {
+  case '1':
   output = Number(firstNumber) + Number(secondNumber);
-} else if (operation === '2') {
+  break;
+  case '2':
   output = Number(firstNumber) - Number(secondNumber);
-} else if (operation === '3') {
+  break;
+  case '3':
   output = Number(firstNumber) * Number(secondNumber);
-} else if (operation === '4') {
+  break;
+  case '4':
   output = Number(firstNumber) / Number(secondNumber);
+  break;
 }
 
-console.log(`the result is: ${output}`);
+prompt(`the result is: ${output}`);
